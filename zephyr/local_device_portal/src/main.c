7d7eb2668868a3c45086cc1997103477373b6189
Alex Burton <82059352+CocoHusky@users.noreply.github.com>
2026-06-30 18:23:46 -0500

    Restore stable direct setup portal

--- zephyr/local_device_portal/src/main.c
+++ zephyr/local_device_portal/src/main.c
@@ -1,24 +1,18 @@
 #include "credential_store.h"
 #include "portal_config.h"
-#include "portal_dns.h"
 #include "portal_http.h"
 #include "portal_state.h"
 #include "wifi_manager.h"
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/net/hostname.h>
-
-#include <string.h>
 
 LOG_MODULE_REGISTER(local_device_portal, LOG_LEVEL_INF);
 
-#define FW_MARKER "zephyr-portal-mdns-captive-2026-06-30-01"
+#define FW_MARKER "zephyr-portal-http-stable-2026-06-30-02"
 
 int main(void)
 {
-	int ret;
-
 	LOG_INF("Local Device Portal Zephyr starting");
 	LOG_INF("FIRMWARE MARKER: %s", FW_MARKER);
 
@@ -26,17 +20,11 @@ int main(void)
 	credential_store_init();
 	wifi_manager_init();
 
-	ret = net_hostname_set(portal_state_host(), strlen(portal_state_host()));
-	if (ret != 0) {
-		LOG_WRN("hostname set failed: %d", ret);
-	}
-	LOG_INF("mDNS/LLMNR hostname: %s.local", portal_state_host());
-
 	LOG_INF("setup Wi-Fi: %s", portal_state_ap_ssid());
 	LOG_INF("setup URL: http://%s/", PORTAL_AP_IP);
 	LOG_INF("dashboard URL target: %s", portal_state_dashboard_url());
 
-	ret = wifi_manager_start_ap();
+	int ret = wifi_manager_start_ap();
 	if (ret != 0) {
 		LOG_ERR("setup AP failed: %d", ret);
 		while (true) {
@@ -44,7 +32,6 @@ int main(void)
 		}
 	}
 
-	portal_dns_start();
 	portal_http_start();
 
 	while (true) {
