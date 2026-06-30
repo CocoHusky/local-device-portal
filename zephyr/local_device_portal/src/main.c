998793cecaa2a2e56b2ec5046d2fae94f48e5f5f
Alex Burton <82059352+CocoHusky@users.noreply.github.com>
2026-06-30 18:32:25 -0500

    Set generated hostname for dashboard

--- zephyr/local_device_portal/src/main.c
+++ zephyr/local_device_portal/src/main.c
@@ -6,13 +6,18 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/net/hostname.h>
+
+#include <string.h>
 
 LOG_MODULE_REGISTER(local_device_portal, LOG_LEVEL_INF);
 
-#define FW_MARKER "zephyr-portal-http-stable-2026-06-30-03"
+#define FW_MARKER "zephyr-portal-dashboard-2026-06-30-01"
 
 int main(void)
 {
+	int ret;
+
 	LOG_INF("Local Device Portal Zephyr starting");
 	LOG_INF("FIRMWARE MARKER: %s", FW_MARKER);
 
@@ -20,11 +25,16 @@ int main(void)
 	credential_store_init();
 	wifi_manager_init();
 
+	ret = net_hostname_set(portal_state_host(), strlen(portal_state_host()));
+	if (ret != 0) {
+		LOG_WRN("hostname set failed: %d", ret);
+	}
+
 	LOG_INF("setup Wi-Fi: %s", portal_state_ap_ssid());
 	LOG_INF("setup URL: http://%s/", PORTAL_AP_IP);
 	LOG_INF("dashboard URL target: %s", portal_state_dashboard_url());
 
-	int ret = wifi_manager_start_ap();
+	ret = wifi_manager_start_ap();
 	if (ret != 0) {
 		LOG_ERR("setup AP failed: %d", ret);
 		while (true) {
